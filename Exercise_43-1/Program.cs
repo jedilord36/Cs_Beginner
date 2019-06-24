0ffe6e86716c5736e8d6fee1277a1ab5784cde69
Eric Spence <ericspence@gmail.com>
2019-06-24 04:55:02 -0700

    adding exercise 43-1

---
+++ Exercise_43-1/Program.cs
@@ -0,0 +1,39 @@
+﻿
+//
+// Udemy Course C# Basics for Beginners
+// Exercise 43-1
+//
+// Write a program and ask the user to enter a number.  The number should be between 1 to 10.
+// If valid, display "Valid" in console.  Otherwise display "Invalid".
+//
+
+using System;
+
+namespace Exercise_43_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numInput = 1;
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a number from 1 to 10: ");
+                numInput = Int32.Parse(Console.ReadLine());
+
+                if (numInput >= 1 && numInput <= 10)
+                {
+                    Console.WriteLine("Valid!");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid!");
+                    break;
+                }
+            }
+
+        }
+    }
+}
